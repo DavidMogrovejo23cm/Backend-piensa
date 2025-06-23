7283f00486ae18fe40da2d3ac4062ccead659312
DavidMogrovejo23cm <ldmogrovejo@sudamericano.edu.ec>
2025-06-23 08:50:33 -0500

    testdefinitive

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
  @Get('/')
-getRoot(): string {
-  return 'API corriendo correctamente';
+getHello(): string {
+  return this.appService.getHello();
 }
 }
